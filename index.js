0b5c10df364011bb66b3d188d73fa5a2f2300880
Ryan Day <soldair@gmail.com>
2015-02-24 10:01:54 -0800

    fixing more inconsistent results without absolute path

--- index.js
+++ index.js
@@ -3,40 +3,40 @@ var parents = require('parents');
 var path = require('path');
 
 module.exports = function(dir,cb){
-  try{
-    var dirs = parents(dir);
-  } catch(e){
-    return process.nextTick(function(){
+
+  fs.realpath(dir,function(err,dir){
+    if(err) return cb(err);
+    try{
+      var dirs = parents(dir);
+    } catch(e){
       cb(e);
-    });
-  }
+    }
 
-  dirs.unshift(dir);
+    (function fn(){
+      if(!dirs.length) cb(new Error('could not find package.json at or above '+dir));
 
-  (function fn(){
-    if(!dirs.length) cb(new Error('could not find package.json at or above '+dir));
+      var d = dirs.shift();
+      var p = path.join(d,'package.json');
+      fs.exists(p,function(exists){
+        if(exists) {
+          cb(false,d);
+        } else {
+          fn();
+        }
+      });
 
-    var d = dirs.shift();
-    var p = path.join(d,'package.json');
-    fs.exists(p,function(exists){
-      if(exists) {
-        fs.realpath(d,function(err,d){
-          cb(err,d);
-        });
-      } else {
-        fn();
-      }
-    });
-
-  })();
+    })();
+  });
 }
 
 module.exports.sync = function(dir){
+  dir = fs.realpathSync(dir);
   var dirs = parents(dir);
-  dirs.unshift(dir);
+  console.log(dirs);
   for(var i=0;i<dirs.length;++i){
     if(fs.existsSync(path.join(dirs[i],'package.json'))){
-      return fs.realpathSync(dirs[i]);
+      return dirs[i];
     }
   }
 }
+
